fb6ecae6f7333a5a2e17c60772fa9bfe3502117d
MaxTrgb <126002400+MaxTrgb@users.noreply.github.com>
2023-11-26 16:19:17 +0200

    Add files via upload

---
+++ C#hw27.11.4/C#hw27.11.4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MySpace
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<string> wordList = new List<string>();
+
+            Console.WriteLine("Enter words (type 'exit' to stop):");
+
+            string userInput;
+            do
+            {
+                userInput = Console.ReadLine();
+
+                if (userInput.ToLower() != "exit")
+                {
+                    // Function to check if a word is not empty
+                    if (IsValidWord(userInput))
+                    {
+                        wordList.Add(userInput);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid word. Please enter a valid word.");
+                    }
+                }
+
+            } while (userInput.ToLower() != "exit");
+
+            Console.WriteLine($"\nNumber of words in the list: {wordList.Count}");
+            Console.WriteLine("Words in the list:");
+
+            foreach (string word in wordList)
+            {
+                Console.WriteLine(word);
+            }
+        }
+
+        private static bool IsValidWord(string word)
+        {
+            return !string.IsNullOrEmpty(word);
+        }
+    }
+}
